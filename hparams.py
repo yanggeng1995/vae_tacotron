e469b99e1b9f591641143d9f754f46b50680cb9c
Geng Yang <gyang@nwpu-aslp.org>
2019-01-21 15:27:05 +0800

    first commit

---
+++ hparams.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+
+
+# Default hyperparameters:
+hparams = tf.contrib.training.HParams(
+  # Comma-separated list of cleaners to run on text prior to training and eval. For non-English
+  # text, you may want to use "basic_cleaners" or "transliteration_cleaners" See TRAINING_DATA.md.
+  cleaners='english_cleaners',
+
+  # Audio:
+  num_mels=80,
+  num_freq=1025,
+  sample_rate=16000,
+  frame_length_ms=50,
+  frame_shift_ms=12.5,
+  preemphasis=0.97,
+  min_level_db=-100,
+  ref_level_db=20,
+
+  # Model:
+  outputs_per_step=5,
+  embed_depth=256,
+  prenet_depths=[256, 128],
+  encoder_depth=256,
+  postnet_depth=256,
+  attention_depth=256,
+  decoder_depth=256,
+
+  #VAE:
+  use_vae=True,
+  vae_dim=32,
+  vae_warming_up=15000,
+  init_vae_weights=0.001, 
+  vae_weight_multiler=0.002,
+  filters=[32, 32, 64, 64, 128, 128],
+ 
+  # Training:
+  batch_size=32,
+  adam_beta1=0.9,
+  adam_beta2=0.999,
+  initial_learning_rate=0.002,
+  decay_learning_rate=True,
+  use_cmudict=False,  # Use CMUDict during training to learn pronunciation of ARPAbet phonemes
+
+  # Eval:
+  max_iters=200,
+  griffin_lim_iters=60,
+  power=1.5,              # Power to raise magnitudes to prior to Griffin-Lim
+)
+
+
+def hparams_debug_string():
+  values = hparams.values()
+  hp = ['  %s: %s' % (name, values[name]) for name in sorted(values)]
+  return 'Hyperparameters:\n' + '\n'.join(hp)
